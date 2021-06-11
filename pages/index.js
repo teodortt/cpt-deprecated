54f51c9723ebb4b4278857fcf6b592eae8d92776
uhb5775 <uhb5775@gmail.com>
2021-06-11 13:27:04 +0300

    pt percent section

--- pages/index.js
+++ pages/index.js
@@ -51,15 +51,30 @@ export default function Home() {
                 <li className="nav-item pl-3"><a href="#" className="nav-link">Attractions</a></li>
                 <li className="nav-item pl-3"><a href="#" className="nav-link">FAQ</a></li>
                 <li className="nav-item pl-3"><a href="#" className="nav-link">Blog</a></li>
-                <li className="nav-item btn-cpt"><a href="#" className="nav-link">Contact</a></li>
+                <li className="nav-item btn-cpt"><a href="#" className="nav-link text-light">Contact</a></li>
               </ul>
             </div>
           </div>
         </nav>
       </header>
 
-      <div className="container-fluid bg-cpt">
-        <div className="pt-5 text-white text-center">
+      <div className="container-fluid bg-cpt pl-0 pr-0">
+
+
+        <video autoplay="true" muted
+          className="video"
+          loop id="myVideo">
+          <source src="https://cdn-cf-east.streamable.com/video/mp4/m7bc6d.mp4?Expires=1623665940&Signature=BiVRzgMKni-exdBmeRWl77mJ5ef0iY7hlUYM8U54Mvc0Pnw4N4smYpj0UdXjrVXpORrDFdSu0dopmb4OkGVKcd-hpsBNg2MXKhOuYRw8KoYNfNB5PUFKPBgMVaBQLtCDMvRa5eZ55VWP-YvOWdYIMfdfoTlJcd-LpL3Q5RNBsQI-rfC1N~JpZ9bRp41yTbPvEW4bCAKvnAaT5iY5dJ5AYoBwCjcwbt7Bqb8ODusWXyAWhXA5yXIZD2Ll8NK5MDR9Mrz0eGQ2N~hlMh0lL30Gxs1vdk4Fd0NrC8TMuTFmKVeR0FK6MuHZ6cLRnmgrz7AOubpZL5Ri8-WEj3jIAgJSbg__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ"
+
+            type="video/mp4" />
+        </video>
+
+
+
+
+
+
+        <div className="pt-5 text-white text-center" style={{ position: "relative" }}>
           <header className="py-5 mt-5">
             <h2 className="display-4">Welcome to</h2>
             <h1 className="" style={{ fontWeight: 900, letterSpacing: '2px', fontSize: '50px' }}>Central Park Tours</h1>
@@ -102,7 +117,7 @@ export default function Home() {
             <div className="ml-5 mr-5">
               <div className="d-flex justify-content-start text-1">Check out our</div>
               <div className="d-flex justify-content-start">
-                <h2 style={{ fontWeight: 800 }}><b style={{ color: '#9bd230' }}>Featured </b>tours</h2>
+                <h2 style={{ fontWeight: 800 }}><b style={{ color: '#88bc2c' }}>Featured </b>tours</h2>
               </div>
               <div className="d-flex justify-content-end font-weight-bold text-uppercase bbottom">View All</div>
 
@@ -163,8 +178,62 @@ export default function Home() {
               <b className="d-flex justify-content-start pl-4">SEE TESTIMONIALS</b>
             </div>
 
+
           </section>
 
+          <section className="about">
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f9f9f8" fill-opacity="1" d="M0,96L80,96C160,96,320,96,480,117.3C640,139,800,181,960,192C1120,203,1280,181,1360,170.7L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+
+            <div className="container-fluid" style={{ background: '#f9f9f8', color: '#707171' }}>
+
+              <div class="fl-box">
+
+                <div class="first text-left mx-auto">
+                  <small className="h4 font-weight-normal" style={{ color: '#535150' }}>Learn more</small>
+                  <h3 className="pt-2 pb-4 h1" style={{ fontWeight: 800, color: '#3d3d3c' }}>About Us</h3>
+                  <p><b>We are thrilled to introduce you to one of the world’s most beautiful parks - Central Park.</b></p>
+
+                  <p>Established in 2003, our company has been the leader in providing bike rentals and various
+                  tours in Central Park. We offer pedicab tours, bicycle tours, bike rentals, picnic
+                  arrangements and horse and carriage rides.</p>
+                  <p>Feel free to take advantage of our attractions page, where you can find a detailed
+                  Central Park map, top things to do in Central Park and of course get access to our
+                  award winning self-guided audio tour of Central Park.</p>
+
+                  <br></br>
+                  <i>Wondering exactly how big is Central Park? Where is the Central Park zoo located at?
+                  Or maybe just looking for interesting facts about the park? Please make sure you
+                  check out our blog!
+                     </i>
+                </div>
+
+                <div class="second">
+                  <img src="/images/about.png" className="about-img" />
+                </div>
+
+              </div>
+
+              <div className="container d-flex text-uppercase" style={{ paddingLeft: 100 }}>
+                <div className="btn-about mr-5">Book a tour</div>
+                <a className="pt-2" href="google.bg"><b className="text-dark">Our Blog</b></a>
+
+              </div>
+
+
+            </div>
+
+
+
+
+            {/* <div class="d-flex justify-content-start text-dark">aaspdosdspdoaspdoaspodaposdopasdopasaaspdosdspdoaspdoaspodaposdopasdopasaaspdosdspdoaspdoaspodaposdopasdopasaaspdosdspdoaspdoaspodaposdopasdopasaaspdosdspdoaspdoaspodaposdopasdopasaaspdosdspdoaspdoaspodaposdopasdopasaaspdosdspdoaspdoaspodaposdopasdopasaaspdosdspdoaspdoaspodaposdopasdopas</div>
+            <div class="d-flex justify-content-end text-dark">...</div> */}
+
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f9f9f8" fill-opacity="1" d="M0,288L80,256C160,224,320,160,480,154.7C640,149,800,203,960,224C1120,245,1280,235,1360,229.3L1440,224L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+
+
+          </section>
 
         </div>
       </div>
